b82719f2b3296e7a30c1faddf7668d14e3c4a61d
gilly <nick.gilmore20@gmail.com>
2023-03-07 14:15:37 -0600

    notes added to the exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,14 +4,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.PrintWriter;
 
+//-- WebServlet: this allows us to specify what URL this servlet should handle.
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+//-- Class definition, Class must extend HttpServlet, Class can be named anything just descriptive
+//-- Commonly suffix with Servlet
 public class HelloWorldServlet extends HttpServlet {
 
+    //-- It is overriding the definition from a parent class.
     @Override
+    // --
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+        //-- Using the object to send some HTML as a response, similar to System.out.println
+        //-- However we us it to print out the response to the browser, instead of console.
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
     }
-
 }
+
+
+//-- THis allows us to extract a value for a parameter submitted with a request.
+
